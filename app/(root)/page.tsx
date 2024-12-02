7ba6159b0eddfe371d349c0463f11f6077bfb36a
Anas Al Saieed <alsaieedanas@gmail.com>
2024-12-02 23:14:02 +0200

    first commit

---
+++ app/(root)/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button";
+import React from "react";
+
+const Homepage = () => {
+  return (
+    <div>
+      <div>Homepage</div>
+      <Button className="bg-primary-500 hover:bg-primary-200">test</Button>
+    </div>
+  );
+};
+
+export default Homepage;
