2a0f3250a6ab9a37ba1e996e9d201967a97c2faa
Anas Al Saieed <alsaieedanas@gmail.com>
2024-12-12 16:51:12 +0200

    day-7-mid-small-update

--- app/(root)/(auth)/login/[email]/page.tsx
+++ app/(root)/(auth)/login/[email]/page.tsx
@@ -1,6 +1,5 @@
 import UserNotFound from "@/components/UserNotFound";
 import VerifyFormSelector from "@/components/VerifyFormSelector";
-import Image from "next/image";
 import React from "react";
 
 const page = ({ params }: { params: { email: string } }) => {
@@ -9,16 +8,6 @@ const page = ({ params }: { params: { email: string } }) => {
   return (
     <div className="h-full w-full sm:w-1/2 font-[600] relative">
       <div className="w-[280px] md:w-[300px] lg:w-[500px] absolute top-[50%] left-1/2 -translate-x-1/2 -translate-y-1/2">
-        <div className="block mb-[10px] sm:hidden">
-          <Image
-            src="/SaveBite.svg"
-            alt="SaveBite"
-            width={329}
-            height={126}
-            className="object-fill"
-          />
-        </div>
-
         <VerifyFormSelector email={email} />
       </div>
       <div className="absolute right-[8px] bottom-[10px]">
--- components/VerifiedSuccess.tsx
+++ components/VerifiedSuccess.tsx
@@ -1,15 +1,18 @@
+"use client";
 import { useVerifyOTP } from "@/contexts/VerifyOTPContext";
 import Image from "next/image";
-import Link from "next/link";
+import { useRouter } from "next/navigation";
 import React from "react";
 
 interface Props {
   email: string;
 }
 const VerifiedSuccess = ({ email }: Props) => {
+  const router = useRouter();
   const { setOTPVerified } = useVerifyOTP()!;
   function handleOnCLick() {
-    setOTPVerified(false);
+    router.push("/login");
+    setTimeout(() => setOTPVerified(false), 1000);
   }
   return (
     <div className="absolute -top-72 w-full">
@@ -21,21 +24,22 @@ const VerifiedSuccess = ({ email }: Props) => {
           height={99}
           className="mx-auto"
         />
-        <span className="text-[48px] font-[600]">Check your email</span>
+        <p className="text-[28px] lg:text-[48px] font-[600] text-center">
+          Check your email
+        </p>
 
-        <div className="mt-[20px] w-[400px] text-center text-[19px] font-[500]">
+        <div className="mt-[20px] w-[300px] lg:w-[400px] text-center text-[19px] font-[500]">
           <span className="text-black-400">
             We have sent an email to{" "}
             <span className="text-black-900">{email}</span> with your image, So
             please Check it now.{" "}
           </span>
-          <button className="mt-[10px]" onClick={handleOnCLick}>
-            <Link
-              href="/login"
-              className="text-primary-500 relative after:content-[''] after:w-full after:h-[1px] after:bg-primary-500 after:absolute after:left-0 after:bottom-[3px]"
-            >
-              back to login
-            </Link>
+          <span className="block"></span>
+          <button
+            className="mt-[10px] text-primary-500 relative after:content-[''] after:w-full after:h-[1px] after:bg-primary-500 after:absolute after:left-0 after:bottom-[3px]"
+            onClick={handleOnCLick}
+          >
+            back to login
           </button>
         </div>
       </div>
