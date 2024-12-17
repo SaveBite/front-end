58df739940a6c16f806af5b60f0d8cb966a9f53d
Anas Al Saieed <alsaieedanas@gmail.com>
2024-12-17 18:39:32 +0200

    day-11-after-otp-error-handling

--- components/Otp.tsx
+++ components/Otp.tsx
@@ -7,6 +7,7 @@ interface Props {
 }
 const Otp = ({ setOTPVerified }: Props) => {
   const [otpCode, setOtpCode] = useState("");
+  const [error, setError] = useState(false);
   function handleOnChange(v: string) {
     if (v.length < 4) return;
     if (v.length === 4) setOtpCode(v);
@@ -14,6 +15,9 @@ const Otp = ({ setOTPVerified }: Props) => {
   function handleOnClick() {
     if (otpCode.length === 4) {
       setOTPVerified(true);
+      setError(false);
+    } else {
+      setError(true);
     }
   }
   return (
@@ -23,19 +27,35 @@ const Otp = ({ setOTPVerified }: Props) => {
           <InputOTPGroup>
             <InputOTPSlot
               index={0}
-              className="sm:p-4 md:p-8 lg:p-12 border-primary-500 focus:ring-0 mr-[10px]"
+              className={`sm:p-4 md:p-8 lg:p-12 ${
+                error
+                  ? "border-error-500 text-error-500"
+                  : "border-primary-500 text-primary-500"
+              } focus:ring-0 mr-[10px]`}
             />
             <InputOTPSlot
               index={1}
-              className="sm:p-4 md:p-8 lg:p-12 border-primary-500 border-l-2 focus:ring-0 mr-[10px]"
+              className={`sm:p-4 md:p-8 lg:p-12 ${
+                error
+                  ? "border-error-500 text-error-500"
+                  : "border-primary-500 text-primary-500"
+              } border-l-2 focus:ring-0 mr-[10px]`}
             />
             <InputOTPSlot
               index={2}
-              className="sm:p-4 md:p-8 lg:p-12 border-primary-500 border-l-2 focus:ring-0 mr-[10px]"
+              className={`sm:p-4 md:p-8 lg:p-12 ${
+                error
+                  ? "border-error-500 text-error-500"
+                  : "border-primary-500 text-primary-500"
+              } border-l-2 focus:ring-0 mr-[10px]`}
             />
             <InputOTPSlot
               index={3}
-              className="sm:p-4 md:p-8 lg:p-12 border-primary-500 border-l-2 focus:ring-0 mr-[10px]"
+              className={`sm:p-4 md:p-8 lg:p-12 ${
+                error
+                  ? "border-error-500 text-error-500"
+                  : "border-primary-500 text-primary-500"
+              } border-l-2 focus:ring-0 mr-[10px]`}
             />
           </InputOTPGroup>
         </InputOTP>
