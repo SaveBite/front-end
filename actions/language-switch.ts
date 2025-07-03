b4df551c479d28fddfc5ec2a961429a6e9a6d541
Anas Al Saieed <alsaieedanas@gmail.com>
2025-07-03 13:16:53 +0300

    landing page finished

--- actions/language-switch.ts
+++ actions/language-switch.ts
@@ -2,5 +2,5 @@
 import { cookies } from "next/headers";
 
 export async function languageSwitch(language: string) {
-  cookies().set("lang", language);
+  (await cookies()).set("lang", language);
 }
--- app/page.tsx
+++ app/page.tsx
@@ -3,11 +3,13 @@ import Blog from "@/components/landingPage/Blog";
 import Features from "@/components/landingPage/Features";
 import Footer from "@/components/landingPage/Footer";
 import Header from "@/components/landingPage/Header";
+import Landing from "@/components/landingPage/Landing";
 
 const Page = () => {
   return (
     <div className="scroll-smooth">
       <Header />
+      <Landing />
       <AboutUs />
       <Features />
       <Blog />
